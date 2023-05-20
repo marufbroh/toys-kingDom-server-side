8acf042ae594561b9aae9fe04189c719b21a1ee4
marufbroh <marufmd775@gmail.com>
2023-05-20 10:45:34 +0600

    single toy details by id api done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -66,6 +66,14 @@ async function run() {
         },
       };
       const result = await toysCollection.find(query, options).toArray();
+      res.send(result);
+    });
+
+    // Single toy details route
+    app.get("/toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.findOne(query);
       res.send(result);
     });
 
