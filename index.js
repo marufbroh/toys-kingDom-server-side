499409cba926f037fcceabb88eadeb4fe9546191
marufbroh <marufmd775@gmail.com>
2023-05-20 13:08:02 +0600

    my toys by req.params.email done

--- index.js
+++ index.js
@@ -77,6 +77,16 @@ async function run() {
       res.send(result);
     });
 
+    // My toys route
+    app.get("/my-toys", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { seller_email: req.query.email };
+      }
+      const result = await toysCollection.find(query).toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
